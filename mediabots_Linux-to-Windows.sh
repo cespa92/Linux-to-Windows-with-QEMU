83539f460380d3f49331a73efd770c87b2bf7ead
cespa92 <40332413+cespa92@users.noreply.github.com>
2022-03-16 00:17:02 -0500

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -36,9 +36,9 @@ link1_status=$(curl -Is http://download.microsoft.com/download/6/2/A/62A76ABB-99
 link2_status=$(curl -Is https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://archive.org/download/WS2012R2/WS2012R2.ISO # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/WS2012R2.ISO http://download.microsoft.com/download/7/6/9/769D6905-3BC7-4CF0-B3BD-785EC88767AF/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_ES-ES.ISO 
+	sudo wget -O /mediabots/WS2012R2.ISO https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_es-es_1.iso 
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://www54.uptobox.com/dl/I9N_lwQpgs3VIxo7fvgv4rRUsM_8__zn_vpVC2nCgz4769C7rzCDnWFofQVCf4JZBKU91Y__W88TRSXCFmfKes7zVVIfIr99R5tXwH8UB06Fb_0Z54NplAicPfkVDhxhaFRg2GMdBizS1ZCxifIEEA/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_ES-ES.ISO
+	sudo wget -P /mediabots https://www41.uptobox.com/dl/nDKuvK1WcYGU_Rk7AVp-3Z1LVqBwXN3Cb267clWIsqkKyIqS72G2Ko0HnOve7cbkwFW6Vp7koifE-6dNHogceKvRssWETCSDZvdyEF423swD5C85vh2Or5xSipTIydnAUrQwVIkJpyhjePW21xgBOnZtH1GxrE_ehGJNWpQsV2E/Windows%20Server%202019.iso
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
